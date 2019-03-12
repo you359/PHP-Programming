18a7b5fc918be9b3b6378bc78ef3b15a76d6f8cd
you359 <you359@koreatech.ac.kr>
2019-03-12 09:51:31 +0900

    upload learning meterials - chapter2

---
+++ chapter2/2-7-avg_5.php
@@ -0,0 +1,14 @@
+﻿<?
+   	$kor = 85;   	// 국어 점수
+	$eng = 90;  	// 영어 점수
+  	$math = 98;  	// 수학 점수
+	$soc = 80;  	// 사회 점수
+ 	$sci = 90;   	// 과학 점수
+
+	$sum = $kor + $eng + $math + $soc + $sci; 	// 5과목의 합계 구하기
+
+        $avg = $sum / 5;
+
+        echo "국어 : $kor, 영어 : $eng, 수학 : $math, 사회 : $soc, 과학 : $sci <br>";
+        echo "합계 : $sum, 평균 : $avg";
+?>
---
+++ chapter2/2-8-fee.php
@@ -0,0 +1,13 @@
+﻿<?
+   	$child = 5000;     	// 청소년 입장료 5,000원
+   	$adult = 8000;		// 성인 입장료 8,000원
+  	$num1 = 3;		// 청소년 3매
+  	$num2 = 2;		// 성인 2매
+
+ 	$total = $child * $num1 + $adult * $num2;
+
+  	echo "청소년 입장료 : $child 원<br>";
+  	echo "성인 입장료 : $adult 원<br>";
+	echo "청소년 : $num1 매, 성인 : $num2 매<br>";
+ 	echo "전체 입장료 : $total 원";
+?>
