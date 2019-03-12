e1b9c13f294cb12ac53ae7711a1b496eb704cc94
you359 <you359@koreatech.ac.kr>
2019-03-12 10:48:58 +0900

    update learning meterial - chapter2

--- chapter2/2-8-fee.php
+++ chapter2/2-8-fee.php
@@ -1,13 +1,17 @@
-﻿<?
-   	$child = 5000;     	// 청소년 입장료 5,000원
-   	$adult = 8000;		// 성인 입장료 8,000원
-  	$num1 = 3;		// 청소년 3매
-  	$num2 = 2;		// 성인 2매
+﻿<?PHP
+/* 영화관 입장료 계산
+ *
+ */
 
- 	$total = $child * $num1 + $adult * $num2;
+$child = 5000;     	// 청소년 입장료 5,000원
+$adult = 8000;		// 성인 입장료 8,000원
+$num1 = 3;		// 청소년 3매
+$num2 = 2;		// 성인 2매
 
-  	echo "청소년 입장료 : $child 원<br>";
-  	echo "성인 입장료 : $adult 원<br>";
-	echo "청소년 : $num1 매, 성인 : $num2 매<br>";
- 	echo "전체 입장료 : $total 원";
+$total = $child * $num1 + $adult * $num2;
+
+echo "청소년 입장료 : $child 원<br>";
+echo "성인 입장료 : $adult 원<br>";
+echo "청소년 : $num1 매, 성인 : $num2 매<br>";
+echo "전체 입장료 : $total 원";
 ?>
--- chapter2/2-8-fee2.php
+++ chapter2/2-8-fee2.php
@@ -1,13 +1,17 @@
-﻿<?
-   	$child = 5000;     	// 청소년 입장료 5,000원
-   	$adult = 8000;		// 성인 입장료 8,000원
-  	$num1 = 3;		// 청소년 3매
-  	$num2 = 2;		// 성인 2매
+﻿<?PHP
+/* 영화관 입장료 계산
+ * $child 와 '원'을 붙여 쓰면? -> #child원 을 합쳐서 변수로 인식함!!
+ */
 
- 	$total = $child * $num1 + $adult * $num2;
+$child = 5000;     	// 청소년 입장료 5,000원
+$adult = 8000;		// 성인 입장료 8,000원
+$num1 = 3;		// 청소년 3매
+$num2 = 2;		// 성인 2매
 
-  	echo "청소년 입장료 : $child원<br>";
-  	echo "성인 입장료 : $adult 원<br>";
-	echo "청소년 : $num1 매, 성인 : $num2 매<br>";
- 	echo "전체 입장료 : $total 원";
+$total = $child * $num1 + $adult * $num2;
+
+echo "청소년 입장료 : $child원<br>";
+echo "성인 입장료 : $adult 원<br>";
+echo "청소년 : $num1 매, 성인 : $num2 매<br>";
+echo "전체 입장료 : $total 원";
 ?>
