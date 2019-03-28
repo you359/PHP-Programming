827ac7e974a97e8a3d089d3d315d8cfe134c56d1
you359 <you359@koreatech.ac.kr>
2019-03-28 13:40:18 +0900

    upload material for experiment (boardz sql file)

---
+++ Experiments/boardz-master/boardz.sql
@@ -0,0 +1,16 @@
+﻿create table boardz(
+  num int auto_increment,
+  image_url char(100),
+  title char(100),
+  contents text,
+  primary key(num)
+);
+
+insert into boardz(image_url, title, contents) values ('http://2.bp.blogspot.com/-pINYV0WlFyA/VUK-QcGbU5I/AAAAAAAABcU/fNy2pd2cFRk/s1600/WEB-Jack-White-Poster-Creative.png', 'PHP', '');
+insert into boardz(image_url, title, contents) values ('http://payload140.cargocollective.com/1/10/349041/5110553/Florrie.jpg', '', '');
+insert into boardz(image_url, title, contents) values ('http://wpmedia.ottawacitizen.com/2015/11/01.jpg?quality=55&strip=all&w=840&h=630&crop=1', 'sumo', '');
+insert into boardz(image_url, title, contents) values ('https://s-media-cache-ak0.pinimg.com/736x/8c/ee/ff/8ceeff967c03c7cf4f86391dd6366544.jpg', '', '');
+insert into boardz(image_url, title, contents) values ('https://s-media-cache-ak0.pinimg.com/originals/87/16/8c/87168cbbf07cb54a9793bebaa20b1bde.jpg', 'sumo', '');
+insert into boardz(image_url, title, contents) values ('https://s-media-cache-ak0.pinimg.com/736x/22/95/48/229548086245c332443109ca9f2e0890.jpg', 'Sumo Summo', 'Ex nostrud verterem mea, duo no delicata neglegentur. Audire integre rationibus ut pri, ex cibo oblique euismod sit, cibo iracundia vix at. Legimus torquatos definiebas an nec, mazim postulant at sit. Ne qui quando vocent accusata, nam tritani fierent no. Ea per vocent voluptatibus.');
+insert into boardz(image_url, title, contents) values ('https://inspirationfeeed.files.wordpress.com/2014/01/ca402f7410884454ec5c303336e8591d1.jpg', '', '');
+
