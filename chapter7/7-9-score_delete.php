bb92c51d956d4c265df803d62cdf2f5672190b67
you359 <you359@koreatech.ac.kr>
2019-03-28 11:23:29 +0900

    update materials - chapter 7 register_globals = Off -> $_GET, $_POST 사용!

--- chapter7/7-9-score_delete.php
+++ chapter7/7-9-score_delete.php
@@ -3,12 +3,15 @@
    mysql_select_db("kyj_db", $connect);
 
    // 필드 num이 $num 값을 가지는 레코드 삭제
-   $sql = "delete from stud_score where num = $num";  
+   $sql = "delete from stud_score where num = $_GET[num]";
    mysql_query($sql, $connect);
 
    mysql_close($connect);
 
-   // 7-8-score_list.php 로 돌아감
-   Header("Location:7-8-score_list.php");
+
 ?>
+<script>
+   // 7-8-score_list.php 로 돌아감
+   location.href = "7-8-score_list.php"
+</script>
  
